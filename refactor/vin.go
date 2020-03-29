4b1f31c715f5bc3aef8bd5c80db0c16fb3e2122f
David <dalau6@gmail.com>
2020-03-29 14:34:22 -0700

    Validity checks in a constructor for type VIN.

--- refactor/vin.go
+++ refactor/vin.go
@@ -1,6 +1,22 @@
 package vin
 
+import "fmt"
+
 type VIN string
+
+// it is debatable if this func should be named New or NewVIN
+// but NewVIN is better for greping and leaves room for other
+// NewXY funcs in the same package
+func NewVIN(code string) (VIN, error) {
+
+	if len(code) != 17 {
+		return "", fmt.Errorf("invalid VIN %s: more or less than 17 characters", code)
+	}
+
+	// ... check for disallowed characters ...
+
+	return VIN(code), nil
+}
 
 func (v VIN) Manufacturer() string {
 
--- refactor/vin_test.go
+++ refactor/vin_test.go
@@ -2,20 +2,31 @@ package vin_test
 
 import (
 	"testing"
-	"vin-stages/2"
 )
 
 const (
-	validVIN   = vin.VIN("W0L000051T2123456")
-	invalidVIN = vin.VIN("W0")
+	validVIN   = "W0L000051T2123456"
+	invalidVIN = "W0"
 )
+
+func TestVIN_New(t *testing.T) {
+
+	_, err := vin.NewVIN(validVIN)
+	if err != nil {
+		t.Errorf("creating valid VIN returned an error: %s", err.Error())
+	}
+
+	_, err = vin.NewVIN(invalidVIN)
+	if err == nil {
+		t.Error("creating invalid VIN did not return an error")
+	}
+}
 
 func TestVIN_Manufacturer(t *testing.T) {
 
-	manufacturer := validVIN.Manufacturer()
+	testVIN, _ := vin.NewVIN(validVIN)
+	manufacturer := testVIN.Manufacturer()
 	if manufacturer != "W0L" {
-		t.Errorf("unexpected manufacturer %s for VIN %s", manufacturer, validVIN)
+		t.Errorf("unexpected manufacturer %s for VIN %s", manufacturer, testVIN)
 	}
-
-	invalidVIN.Manufacturer() // panic!
 }
